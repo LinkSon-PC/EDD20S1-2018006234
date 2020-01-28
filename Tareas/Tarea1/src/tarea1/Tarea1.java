5b206e88222644e7e7707db702f0e57cb8b04dba
Anthony Son <tonyfernandosonmux@gmail.com>
2020-01-27 19:15:30 -0600

    Matriz M*M
    Impresion de marco de una matriz de orden M

---
+++ Tareas/Tarea1/src/tarea1/Tarea1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Home
+ */
+public class Tarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Escoja el orden de la Matriz");
+
+        try {
+            
+            int M;
+            Scanner scanner = new Scanner(System.in);
+            M = scanner.nextInt();
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (i==0 || i==(M-1) || j==0 || j==(M-1)) {
+                        System.out.print("|1|");
+                    }else{
+                        System.out.print("| |");
+                    }
+                    System.out.print("\t");
+                }
+                System.out.println();
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Orden de matriz incorrecto");
+        }
+
+    }
+    
+    
+}
